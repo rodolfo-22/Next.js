2fbd6bbf92cbb4c33c5f32b691d58dd9bfaa39af
rodolfo-22 <100951955+rodolfo-22@users.noreply.github.com>
2024-07-24 02:48:51 -0600

    :zap: add view

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,46 @@
-import React from 'react'
+    import { titleFont } from '@/config/font';
+    import Link from 'next/link';
 
-const Page = () => {
-  return (
-    <>
-        <h1 className="text-4xl font-bold">Login</h1>
-        <p className="text-lg">This is the login page </p>
-    </>
-  )
-}
+    export default function LoginPage() {
+    return (
+        <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
 
-export default Page
+        <h1 className={ `${ titleFont.className } text-4xl mb-5` }>Ingresar</h1>
+
+        <div className="flex flex-col">
+
+            <label htmlFor="email">Correo electrónico</label>
+            <input
+            className="px-5 py-2 border bg-gray-200 rounded mb-5"
+            type="email" />
+
+
+            <label htmlFor="email">Contraseña</label>
+            <input
+            className="px-5 py-2 border bg-gray-200 rounded mb-5"
+            type="email" />
+
+            <button
+            
+            className="btn-primary">
+            Ingresar
+            </button>
+
+
+            {/* divisor l ine */ }
+            <div className="flex items-center my-5">
+            <div className="flex-1 border-t border-gray-500"></div>
+            <div className="px-2 text-gray-800">O</div>
+            <div className="flex-1 border-t border-gray-500"></div>
+            </div>
+
+            <Link
+            href="/auth/new-account" 
+            className="btn-secondary text-center">
+            Crear una nueva cuenta
+            </Link>
+
+        </div>
+        </div>
+    );
+    }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import { titleFont } from '@/config/font';
+import Link from 'next/link';
 
-const page = () => {
+export default function NewAccountPage() {
   return (
-    <>
-        <h1 className="text-4xl font-bold">Register</h1>
-        <p className="text-lg">This is the register page </p>
-    </>
-  )
-}
+    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
 
-export default page
+      <h1 className={ `${ titleFont.className } text-4xl mb-5` }>Crea una cuenta</h1>
+
+      <div className="flex flex-col">
+        
+      <label htmlFor="email">Nombre completo</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="text" />
+
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="email" />
+
+
+        <label htmlFor="email">Contraseña</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="email" />
+
+        <button
+          
+          className="btn-primary">
+          Crear cuenta
+        </button>
+
+
+        {/* divisor l ine */ }
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">O</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <Link
+          href="/auth/login" 
+          className="btn-secondary text-center">
+          Ingresar
+        </Link>
+
+      </div>
+    </div>
+  );
+}
